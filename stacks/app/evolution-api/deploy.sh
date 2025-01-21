bc8133ba8941c7938724dbb8382dea66f1a155de
Felipe Fontoura <ffontouras@gmail.com>
2025-01-21 16:53:28 -0300

    fix path

--- stacks/app/evolution-api/deploy.sh
+++ stacks/app/evolution-api/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker stack deploy --prune --resolve-image always --compose-file ./app/evolution-api/stack.yml evolution-api
+docker stack deploy --prune --resolve-image always --compose-file ./stacks/app/evolution-api/stack.yml evolution-api
